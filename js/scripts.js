bcac7279f27e33869045017f817337242defc086
Isaac Hall and sean byram <isaac@>
2019-01-15 15:45:10 -0800

    initial flash cards, this .children toggles javascript solution

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+  // var bool = true;
+  //
+  // $(".clickable").click(function() {
+  //   if(bool){
+  //     $("#walrus-showing").fadeIn();
+  //   } else {
+  //     $("#walrus-showing").fadeOut();
+  //   };
+  //   bool = !bool;
+  // });
+  //
+  //
+  //
+  // $("button#primary").click(function() {
+  //   $("body").removeClass();
+  //   $("body").addClass("primary-background");
+  // });
+
+
+
+$(".switch").click(function() {
+  $(this).children(".front").toggle();
+  $(this).children(".back").toggle();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
